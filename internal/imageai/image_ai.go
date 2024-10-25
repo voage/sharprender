bddcae331c92b2b37dd43af8f09ceed7a6421652
rupokghosh <rupokghosh291@gmail.com>
2024-10-25 14:07:11 -0700

    add boilerplate structs and functions

--- internal/imageai/image_ai.go
+++ internal/imageai/image_ai.go
@@ -1,6 +1,21 @@
 package imageai
 
-func GenerateRecommendations() {
+func GenerateRecommendations(i Image) {
 
 	// gonna generate some awesome recommendations
+	// first do it for one image ( use for loop to loop through images and then repeatedly call this function for responses)
+	// add concurrency later
 }
+
+func prompt (i Image) string {
+	// returns prompt with all image info that we are gonna ask gpt
+}
+
+func apiCall () {
+	// api req to openai
+}
+
+func handleResponse() {
+	// parse raw response into usable recommendations that we can display on the frontend
+}
+
--- internal/imageai/models.go
+++ internal/imageai/models.go
@@ -1,1 +1,13 @@
 package imageai
+
+type Recommendation struct {
+	Title string `json:"title"`
+	Details string `json:"details"`
+
+}
+type AIRequest struct {
+	Recommendations []Recommendation `json:"rec"`
+}
+type AIResponse struct {
+	Recs []Recommendation `json:"recs"`
+}
