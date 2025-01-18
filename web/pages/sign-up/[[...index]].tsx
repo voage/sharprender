5b7f8c82dd71cb062e3d89355850a39b043774ce
rupokghosh <rupokghosh291@gmail.com>
2025-01-18 11:04:51 -0500

    Update auth pages with new components

--- web/pages/sign-up/[[...index]].tsx
+++ web/pages/sign-up/[[...index]].tsx
@@ -1,5 +1,16 @@
 import { SignUp } from "@clerk/nextjs";
+import AuthLayout from "@/components/Auth/auth_layout";
+import { clerkAppearance } from "@/lib/clerk_appearance";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <AuthLayout
+      title="Welcome Back"
+      description="Sign in to your account to continue"
+    >
+      <div className="w-full max-w-md mx-auto p-4 sm:p-6 lg:p-8">
+        <SignUp appearance={clerkAppearance} />
+      </div>
+    </AuthLayout>
+  );
 }
