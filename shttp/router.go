8739e55a7344068adf54fb796b43e828a55bdf4b
rupokghosh <rupokghosh291@gmail.com>
2025-01-20 21:15:02 -0500

    Add CORS support

--- shttp/router.go
+++ shttp/router.go
@@ -2,12 +2,21 @@ package shttp
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/voage/sharprender-api/db"
 	"github.com/voage/sharprender-api/shttp/scan"
 )
 
 func NewRouter(mongoClient *db.MongoClient) *chi.Mux {
 	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"}, // Allow your frontend's origin
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		ExposedHeaders:   []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           300, // Cache preflight response for 5 minutes
+	}))
 	router.Mount("/scan", scan.NewScanRoutes(mongoClient.Client))
 
 	return router
